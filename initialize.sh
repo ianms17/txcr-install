c76c0ac34aba1520bdadf30fbbd48ebe73a30ed8
ianms17 <35043635+ianms17@users.noreply.github.com>
2021-09-19 16:16:32 -0500

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update
+sudo apt install curl -y
+sudo apt install php7.4-cli -y
 sudo apt-get install g++ -y
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install python3.6 -y
@@ -8,7 +10,7 @@ sudo snap install pycharm-community --classic -y
 sudo snap install intellij-idea-community --classic -y
 sudo apt-get install evince
 sudo apt install libreoffice
+sudo apt install wordpress -y
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp-cli
-wp-cli core download
+sudo mv wp-cli.phar /usr/local/bin/wp
