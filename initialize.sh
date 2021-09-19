a6f2e93cf1901b18c831bf93852f657a4b298a5e
Ian Stephenson <ianmstephenson17@gmail.com>
2021-09-19 15:28:33 -0500

    Initial commit

---
+++ initialize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install g++ -y
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install python3.6 -y
+sudo snap install code --classic -y
+sudo snap install pycharm-community --classic -y
+sudo snap install intellij-idea-community --classic -y
+sudo apt-get install evince
+sudo apt install libreoffice
